ab51ed43541b40719b30dc617c1957d9676cea7f
queeniexiang <wxiang1@stuy.edu>
2018-01-09 11:41:58 -0500

    Finsihed

---
+++ generate_randdir.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+
+
+int main() {
+  int rand_num = Math.random
