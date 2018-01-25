b59451c4db417e9fd7b7d80a1a045051aa6e814f
TerryMinal <tguan@stuy.edu>
2018-01-25 12:38:59 -0500

    updated main

--- main.c
+++ main.c
@@ -8,19 +8,23 @@ void game_setup() {
   while (fgets(str,sizeof(str), stdin) ) {
     if (strcmp(str, "1")) {
       char res[5];
-      printf("please create a server by running [make server] in a new terminal\n");
+      printf("Please create a server by running [make server] in a new terminal\n");
       printf("You will need to acquire your IP address. You can do this by running [ifconfig]\n1: Done\n");
       fgets(res, sizeof(res), stdin);
       if (strcmp(res, "1")) { // user should've set up server by now
         client_setup("127.0.0.1"); // connects client to local
+        printf("Connected to localhost\n");
+        while(1) {} //debugging purposes
         break;
       }
     } //end of host game
     else if (strcmp(str, "2")) {
       char res[100];
-      printf("enter ip address/domain of server:\n");
+      printf("Enter ip address/domain of server:\n");
       fgets(res, sizeof(res), stdin);
       client_setup(res); //connect client to hosting computer
+      printf("connected to host\n");
+      while(1) {} //debugging purposes
       break;
     } //end of join game
     else {
